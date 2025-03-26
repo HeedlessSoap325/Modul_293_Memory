df7c5d52f7451d0e4fe5e6302b2b54baaa4cf699
HeedlessSoap325 <mathisjulian123@gmail.com>
2025-03-26 14:21:25 +0100

    Einfügen für Testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css'
+import GameCard from "./components/GameCard.jsx";
 
 export default function App() {
     return (
         <>
+            <GameCard imageName={"test.jpg"}/>
         </>
     );
 }
